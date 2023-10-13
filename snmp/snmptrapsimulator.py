3d5f263d18f2b2ba686834e9c6b2a42a4bfe025a
sriprasad123 <32750005+sriprasad123@users.noreply.github.com>
2023-10-13 13:05:24 +0530

    Update snmptrapsimulator.py

--- snmp/snmptrapsimulator.py
+++ snmp/snmptrapsimulator.py
@@ -14,19 +14,25 @@ def send_snmpv3_trap():
     snmp_user = UsmUserData(user, auth_protocol, None, priv_protocol, None)
     snmp_target = UdpTransportTarget((receiver_ip, receiver_port))
 
-    with NotificationOriginator(snmp_engine, snmp_user) as notification_originator:
-        var_binds = (
-            ('1.3.6.1.2.1.1.1.0', OctetString('TestSystem')),
-            ('1.3.6.1.4.1.12345.0.1', Integer(42))
-        )
-        errorIndication, errorStatus, errorIndex, varBindTable = notification_originator.sendVarBinds(
-            snmp_engine, 'trap', var_binds
-        )
+    var_binds = (
+        ('1.3.6.1.2.1.1.1.0', OctetString('TestSystem')),
+        ('1.3.6.1.4.1.12345.0.1', Integer(42))
+    )
 
-        if errorIndication:
-            print(f'Failed to send SNMP trap: {errorIndication}')
-        else:
-            print('SNMP trap sent successfully')
+    errorIndication, errorStatus, errorIndex, varBindTable = next(
+        sendNotification(
+            snmp_engine,
+            snmp_user,
+            None,
+            'trap',
+            var_binds=var_binds
+        )
+    )
+
+    if errorIndication:
+        print(f'Failed to send SNMP trap: {errorIndication}')
+    else:
+        print('SNMP trap sent successfully')
 
 if __name__ == '__main__':
     send_snmpv3_trap()
